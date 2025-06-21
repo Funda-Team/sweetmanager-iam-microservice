f7962b10596b132fdd314fbd316200c018e44b57
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-21 17:56:39 -0500

    feat(facade): added iam services.

---
+++ IamService/Interfaces/ACL/IIamContextFacade.cs
@@ -0,0 +1,13 @@
+﻿using IamService.Domain.Model.Aggregates;
+
+namespace IamService.Interfaces.ACL
+{
+    public interface IIamContextFacade
+    {
+        Task<Admin?> FetchAdminByUserId(int id);
+
+        Task<Owner?> FetchOwnerByUserId(int id);
+
+        Task<Worker?> FetchWorkerByUserId(int id);
+    }
+}
---
+++ IamService/Interfaces/ACL/Services/IamContextFacade.cs
@@ -0,0 +1,21 @@
+﻿using IamService.Domain.Model.Aggregates;
+using IamService.Domain.Model.Queries;
+using IamService.Domain.Services.Users.Admin;
+using IamService.Domain.Services.Users.Owner;
+using IamService.Domain.Services.Users.Worker;
+
+namespace IamService.Interfaces.ACL.Services
+{
+    public class IamContextFacade(IAdminQueryService adminQueryService, IOwnerQueryService ownerQueryService,
+        IWorkerQueryService workerQueryService) : IIamContextFacade
+    {
+        public async Task<Admin?> FetchAdminByUserId(int id)
+            => await adminQueryService.Handle(new GetUserByIdQuery(id));
+
+        public async Task<Owner?> FetchOwnerByUserId(int id)
+            => await ownerQueryService.Handle(new GetUserByIdQuery(id));
+
+        public async Task<Worker?> FetchWorkerByUserId(int id)
+            => await workerQueryService.Handle(new GetUserByIdQuery(id));
+    }
+}
